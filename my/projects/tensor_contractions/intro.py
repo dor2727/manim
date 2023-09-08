acafc461aaab51492735ca931e6ac0177de744cf
Dor Harpaz <harpazdor@gmail.com>
2023-09-08 16:59:16 +0300

    Finish `IntroActions`

--- my/projects/tensor_contractions/intro.py
+++ my/projects/tensor_contractions/intro.py
@@ -8,7 +8,7 @@ from consts import (
 
 from manim import *
 
-spacing_horizontal = 3.5
+spacing_horizontal = 4.5
 
 
 def create_body(shape: str = "square", size: float = DEFAULT_DOT_RADIUS * 3):
@@ -190,10 +190,28 @@ class IntroActions(Scene):
         self.sub_scene_dot_product()
         self.wait(1)
 
-        # self.sub_scene_matrix_vector_multiplication()
+        # Text: Likewise,
+        #       A vector is represented with 1 leg
+        #       < pause >
+        #       And the result of:
+        #         - matrix
+        #         - vector
+        #         - multiplication
+        #       < pause >
+        #       Is also a vector, which is a one-legged object
+        self.sub_scene_matrix_vector_multiplication()
         self.wait(1)
 
-        # self.sub_scene_matrix_matrix_multiplication()
+        # Text: And, similarly,
+        #       A matrix is represented with 2 legs
+        #       < pause >
+        #       And the result of:
+        #         - matrix
+        #         - matrix
+        #         - multiplication
+        #       < pause >
+        #       Is also a matrix, with 2 legs sticking out
+        self.sub_scene_matrix_matrix_multiplication()
         self.wait(1)
 
     def sub_scene_dot_product(self):
@@ -229,330 +247,66 @@ class IntroActions(Scene):
         self.play(Create(surrounding_rectangle), run_time=0.5)
         self.wait(1)
 
-
-class SimpleActions(Scene):
-    def construct(self):
-        self.sub_scene_dot_product()
-
-        self.wait(1)
-
-        self.sub_scene_matrix_vector_multiplication()
-
-        self.wait(1)
-
-        self.sub_scene_matrix_matrix_multiplication()
-        # self.sub_scene_outer_product()
-
-    def sub_scene_dot_product(self):
-        # Text: For example,
-        #       We can represent a number as a circle with no legs
-
-        tnn_number = Dot(color=CIRCLE_COLOR)
-        tnn_number.shift(spacing_horizontal * LEFT + UP)
-        self.play(Create(tnn_number), run_time=0.5)
-
-        self.wait(1)
-
-        # Text: Likewise, taking 2 vectors
-
-        tnn_vector_left_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector_left = VMobject()
-        tnn_vector_left.add(
-            tnn_vector_left_dot,
-            Line(
-                start=tnn_vector_left_dot.get_right(),
-                end=tnn_vector_left_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-        )
-        tnn_vector_left.shift((spacing_horizontal + 1) * LEFT + DOWN)
-
-        tnn_vector_right_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector_right = VMobject()
-        tnn_vector_right.add(
-            tnn_vector_right_dot,
-            Line(
-                start=tnn_vector_right_dot.get_left(),
-                end=tnn_vector_right_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
-        )
-        tnn_vector_right.shift((spacing_horizontal - 1) * LEFT + DOWN)
-
-        self.play(Create(tnn_vector_left), Create(tnn_vector_right), run_time=0.5)
-
-        self.wait(1)
-
-        # Text: And taking their dot product
-        dot_product_line = Line(
-            start=tnn_vector_left.get_right(), end=tnn_vector_right.get_left()
-        )
-
-        self.play(Create(dot_product_line), run_time=0.5)
-
-        self.wait(1)
-
-        # Text: Gives us something that's also an object with no legs
-        rectangle_right_edge = tnn_vector_right.get_right() + 0.5 * RIGHT
-        rectangle_left_edge = tnn_vector_left.get_left() + 0.5 * LEFT
-        rectangle_center_height = DOWN
-
-        surrounding_rectangle = Rectangle(
-            height=(DEFAULT_DOT_RADIUS * 2 + 0.5) * 2,
-            width=np.linalg.norm(rectangle_right_edge - rectangle_left_edge),
-            color=RED_B,
-        )
-        surrounding_rectangle.move_to(dot_product_line.get_center())
-
-        self.play(Create(surrounding_rectangle), run_time=0.5)
-
-        self.wait(1)
-
     def sub_scene_matrix_vector_multiplication(self):
-        # Text: Likewise,
-        #       A vector is represented with 1 leg
-        tnn_vector_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector = VMobject()
-        tnn_vector.add(
-            tnn_vector_dot,
-            Line(
-                start=tnn_vector_dot.get_left(),
-                end=tnn_vector_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
-        )
-        tnn_vector.shift(UP)
+        tnn_vector_representation = create_vector_left()
+        tnn_vector_representation.shift(UP)
 
+        self.play(Create(tnn_vector_representation), run_time=0.5)
+        self.wait(1)
+
+        tnn_matrix = create_matrix_horizontal(left_leg_length=INITIAL_LEG_LENGTH + 0.3)
+        tnn_matrix.shift(LEFT + DOWN)
+        self.play(Create(tnn_matrix), run_time=0.5)
+        self.wait(0.25)
+
+        tnn_vector = create_vector_left()
+        tnn_vector.shift(RIGHT + DOWN)
         self.play(Create(tnn_vector), run_time=0.5)
+        self.wait(0.5)
 
-        self.wait(1)
-
-        # Text: And the result of matrix-vector multiplication
-
-        # Maybe show each part separately
-        #   matrix (show matrix)
-        #   vector (show vector)
-        #   multiplication (show line)
-
-        tnn_vector_left_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector_left = VMobject()
-        tnn_vector_left.add(
-            tnn_vector_left_dot,
-            Line(
-                start=tnn_vector_left_dot.get_right(),
-                end=tnn_vector_left_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-        )
-        tnn_vector_left.shift(LEFT + DOWN)
-
-        tnn_matrix_dot = Dot(color=CIRCLE_COLOR)
-        tnn_matrix = VMobject()
-        tnn_matrix.add(
-            tnn_matrix_dot,
-            Line(
-                start=tnn_matrix_dot.get_right(),
-                end=tnn_matrix_dot.get_right() + (INITIAL_LEG_LENGTH + 0.8) * RIGHT,
-            ),
-            Line(
-                start=tnn_matrix_dot.get_left(),
-                end=tnn_matrix_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
-        )
-        tnn_matrix.shift(RIGHT + DOWN)
-
-        self.play(Create(tnn_vector_left), Create(tnn_matrix), run_time=0.5)
-
-        self.wait(1)
-
-        dot_product_line = Line(
-            start=tnn_vector_left.get_right(), end=tnn_matrix.get_left()
-        )
-
+        dot_product_line = create_connecting_horizontal_line(tnn_matrix, tnn_vector)
         self.play(Create(dot_product_line), run_time=0.5)
-
         self.wait(1)
 
-        # Text: Is also a vector, which is a one-legged object
-        rectangle_right_edge = (
-            tnn_matrix.get_right() + 0.4 * LEFT
-        )  # a bit to the left, cutting the matrix
-        rectangle_left_edge = tnn_vector_left.get_left() + 0.5 * LEFT
-        rectangle_center_height = DOWN
-
-        surrounding_rectangle = Rectangle(
-            height=(DEFAULT_DOT_RADIUS * 2 + 0.5) * 2,
-            width=np.linalg.norm(rectangle_right_edge - rectangle_left_edge),
-            color=RED_B,
+        surrounding_rectangle = create_surrounding_horizontal_rectangle(
+            tnn_matrix, tnn_vector, extra_width=0
         )
         surrounding_rectangle.move_to(dot_product_line.get_center())
 
         self.play(Create(surrounding_rectangle), run_time=0.5)
-
         self.wait(1)
 
     def sub_scene_matrix_matrix_multiplication(self):
-        tnn_matrix_dot = Dot(color=CIRCLE_COLOR)
-        tnn_matrix = VMobject()
-        tnn_matrix.add(
-            tnn_matrix_dot,
-            Line(
-                start=tnn_matrix_dot.get_right(),
-                end=tnn_matrix_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-            Line(
-                start=tnn_matrix_dot.get_left(),
-                end=tnn_matrix_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
-        )
-        tnn_matrix.shift(UP)
+        tnn_matrix_representation = create_matrix_horizontal()
+        tnn_matrix_representation.shift(spacing_horizontal * RIGHT + UP)
 
-        self.play(Create(tnn_matrix), run_time=0.5)
-
+        self.play(Create(tnn_matrix_representation), run_time=0.5)
         self.wait(1)
 
-        # Text: And the result of matrix-vector multiplication
-
-        # Maybe show each part separately
-        #   matrix (show matrix)
-        #   vector (show vector)
-        #   multiplication (show line)
-
-        tnn_matrix_left_dot = Dot(color=CIRCLE_COLOR)
-        tnn_matrix_left = VMobject()
-        tnn_matrix_left.add(
-            tnn_matrix_left_dot,
-            Line(
-                start=tnn_matrix_left_dot.get_right(),
-                end=tnn_matrix_left_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-            Line(
-                start=tnn_matrix_left_dot.get_left(),
-                end=tnn_matrix_left_dot.get_left() + (INITIAL_LEG_LENGTH + 0.8) * LEFT,
-            ),
+        tnn_matrix_left = create_matrix_horizontal(
+            left_leg_length=INITIAL_LEG_LENGTH + 0.3
         )
-        tnn_matrix_left.shift(LEFT + DOWN)
+        tnn_matrix_left.shift(spacing_horizontal * RIGHT + LEFT + DOWN)
+        self.play(Create(tnn_matrix_left), run_time=0.5)
+        self.wait(0.25)
 
-        tnn_matrix_right_dot = Dot(color=CIRCLE_COLOR)
-        tnn_matrix_right = VMobject()
-        tnn_matrix_right.add(
-            tnn_matrix_right_dot,
-            Line(
-                start=tnn_matrix_right_dot.get_right(),
-                end=tnn_matrix_right_dot.get_right()
-                + (INITIAL_LEG_LENGTH + 0.8) * RIGHT,
-            ),
-            Line(
-                start=tnn_matrix_right_dot.get_left(),
-                end=tnn_matrix_right_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
+        tnn_matrix_right = create_matrix_horizontal(
+            right_leg_length=INITIAL_LEG_LENGTH + 0.3
         )
-        tnn_matrix_right.shift(RIGHT + DOWN)
+        tnn_matrix_right.shift(spacing_horizontal * RIGHT + RIGHT + DOWN)
+        self.play(Create(tnn_matrix_right), run_time=0.5)
+        self.wait(0.5)
 
-        self.play(Create(tnn_matrix_left), Create(tnn_matrix_right), run_time=0.5)
-
-        self.wait(1)
-
-        dot_product_line = Line(
-            start=tnn_matrix_left.get_right(), end=tnn_matrix_right.get_left()
+        dot_product_line = create_connecting_horizontal_line(
+            tnn_matrix_left, tnn_matrix_right
         )
-
         self.play(Create(dot_product_line), run_time=0.5)
-
         self.wait(1)
 
-        # Text: Is also a vector, which is a one-legged object
-        rectangle_right_edge = (
-            tnn_matrix_right.get_right() + 0.4 * LEFT
-        )  # a bit to the left, cutting the matrix
-        rectangle_left_edge = tnn_matrix_left.get_left() + 0.5 * LEFT
-        rectangle_center_height = DOWN
-
-        surrounding_rectangle = Rectangle(
-            height=(DEFAULT_DOT_RADIUS * 2 + 0.5) * 2,
-            width=np.linalg.norm(rectangle_right_edge - rectangle_left_edge),
-            color=RED_B,
+        surrounding_rectangle = create_surrounding_horizontal_rectangle(
+            tnn_matrix_left, tnn_matrix_right, extra_width=-0.3
         )
         surrounding_rectangle.move_to(dot_product_line.get_center())
 
         self.play(Create(surrounding_rectangle), run_time=0.5)
-
-        self.wait(1)
-
-    def sub_scene_outer_product(self):
-        # Text: As a last example,
-        #       The outer product is a less familiar operation
-        #       With a very simple TNN diagram
-
-        tnn_matrix_dot = Dot(color=CIRCLE_COLOR)
-        tnn_matrix = VMobject()
-        tnn_matrix.add(
-            tnn_matrix_dot,
-            Line(
-                start=tnn_matrix_dot.get_right(),
-                end=tnn_matrix_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-            Line(
-                start=tnn_matrix_dot.get_left(),
-                end=tnn_matrix_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
-        )
-        tnn_matrix.shift(spacing_horizontal * RIGHT + UP)
-
-        self.play(Create(tnn_matrix), run_time=0.5)
-
-        self.wait(1)
-
-        # Text: You start by taking 2 vectors
-
-        tnn_vector_pointing_left_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector_pointing_left = VMobject()
-        tnn_vector_pointing_left.add(
-            tnn_vector_pointing_left_dot,
-            Line(
-                start=tnn_vector_pointing_left_dot.get_left(),
-                end=tnn_vector_pointing_left_dot.get_left()
-                + (INITIAL_LEG_LENGTH + 0.3) * LEFT,
-            ),
-        )
-        tnn_vector_pointing_left.shift((spacing_horizontal - 0.5) * RIGHT + DOWN)
-
-        tnn_vector_pointing_right_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector_pointing_right = VMobject()
-        tnn_vector_pointing_right.add(
-            tnn_vector_pointing_right_dot,
-            Line(
-                start=tnn_vector_pointing_right_dot.get_right(),
-                end=tnn_vector_pointing_right_dot.get_right()
-                + (INITIAL_LEG_LENGTH + 0.3) * RIGHT,
-            ),
-        )
-        tnn_vector_pointing_right.shift((spacing_horizontal + 0.5) * RIGHT + DOWN)
-
-        self.play(
-            Create(tnn_vector_pointing_left),
-            Create(tnn_vector_pointing_right),
-            run_time=0.5,
-        )
-
-        self.wait(1)
-
-        # Text: and that's it. Here's a matrix.
-        rectangle_right_edge = (
-            tnn_vector_pointing_right.get_right() + 0.3 * LEFT
-        )  # a bit to the left, cutting the vector
-        rectangle_left_edge = (
-            tnn_vector_pointing_left.get_left() + 0.3 * RIGHT
-        )  # a bit to the right, cutting the vector
-        rectangle_center_height = DOWN
-        rectangle_center = (
-            tnn_vector_pointing_right.get_center()
-            + tnn_vector_pointing_left.get_center()
-        ) / 2
-
-        surrounding_rectangle = Rectangle(
-            height=(DEFAULT_DOT_RADIUS * 2 + 0.5) * 2,
-            width=np.linalg.norm(rectangle_right_edge - rectangle_left_edge),
-            color=RED_B,
-        )
-        surrounding_rectangle.move_to(rectangle_center)
-
-        self.play(Create(surrounding_rectangle), run_time=0.5)
-
         self.wait(1)
