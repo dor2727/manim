2452213cfc6aa0bd6afe729a1680caf06d941a4f
Dor Harpaz <harpazdor@gmail.com>
2023-09-08 16:35:52 +0300

    Clean `Intro`

--- my/projects/tensor_contractions/intro.py
+++ my/projects/tensor_contractions/intro.py
@@ -26,11 +26,17 @@ def create_body(shape: str = "square", size: float = DEFAULT_DOT_RADIUS * 3):
 
 
 def create_left_leg(body: Mobject, leg_length: float = INITIAL_LEG_LENGTH):
-    return Line(start=body.get_left(), end=body.get_left() + leg_length * LEFT)
+    return Line(
+        start=body.get_left(),
+        end=body.get_left() + leg_length * LEFT,
+    )
 
 
 def create_right_leg(body: Mobject, leg_length: float = INITIAL_LEG_LENGTH):
-    return Line(start=body.get_right(), end=body.get_right() + leg_length * RIGHT)
+    return Line(
+        start=body.get_right(),
+        end=body.get_right() + leg_length * RIGHT,
+    )
 
 
 def create_vector_left(shape="square", leg_length: float = INITIAL_LEG_LENGTH):
@@ -58,7 +64,7 @@ def create_matrix_horizontal(
     return matrix
 
 
-class Intro_2(Scene):
+class Intro(Scene):
     def construct(self):
         # Text: We will work with
         #       - Numbers
@@ -90,7 +96,6 @@ class Intro_2(Scene):
         self.some_number = some_number = Tex("2.4")
         some_number.shift(spacing_horizontal * LEFT)
 
-        # some_vector = Tex("$$ \\begin{pmatrix} 1 \\\\ 2 \\\\ 3 \\end{pmatrix} $$")
         self.some_vector = some_vector = Matrix([[1], [2], [3]])
 
         self.some_matrix = some_matrix = Matrix([["\\pi", 5], [-1, "e"]])
@@ -129,116 +134,15 @@ class Intro_2(Scene):
         self.wait(0.5)
 
     def change_tnn_objects_body(self):
-        different_tnn_vector_body = create_body("square")
-        original_tnn_vector_body = self.tnn_vector.submobjects[0]
+        self._change_body(self.tnn_vector, create_body("square"))
 
-        different_tnn_vector_body.move_to(original_tnn_vector_body.get_center())
-        self.play(Transform(original_tnn_vector_body, different_tnn_vector_body))
+        self._change_body(self.tnn_matrix, create_body("diamond"))
 
-        different_tnn_matrix_body = create_body("diamond")
-        original_tnn_matrix_body = self.tnn_matrix.submobjects[0]
+    def _change_body(self, original_vmobject, new_body):
+        original_body = original_vmobject.submobjects[0]
 
-        different_tnn_matrix_body.move_to(original_tnn_matrix_body.get_center())
-        self.play(Transform(original_tnn_matrix_body, different_tnn_matrix_body))
-
-
-class Intro(Scene):
-    def construct(self):
-        # Text: We will work with
-        #       - Numbers
-        #       - Vectors
-        #       - and Matrices
-
-        some_number = Tex("2.4")
-        some_number.shift(spacing_horizontal * LEFT)
-
-        some_vector = Tex("$$ \\begin{pmatrix} 1 \\\\ 2 \\\\ 3 \\end{pmatrix} $$")
-
-        some_matrix = Matrix([["\\pi", 5], [-1, "e"]])
-        some_matrix.shift(spacing_horizontal * RIGHT)
-
-        self.play(Create(some_number), run_time=0.5)
-        self.wait(0.5)
-        self.play(Create(some_vector), run_time=0.5)
-        self.wait(0.5)
-        self.play(Create(some_matrix), run_time=0.5)
-        self.wait(0.5)
-
-        self.wait(1)
-
-        # Text: Though we will represent them as
-        self.play(
-            some_number.animate.shift(2 * UP),
-            some_vector.animate.shift(2 * UP),
-            some_matrix.animate.shift(2 * UP),
-            run_time=1.5,
-        )
-
-        self.wait(1)
-
-        tnn_number = Dot(color=CIRCLE_COLOR)
-        tnn_number.shift(spacing_horizontal * LEFT + DOWN)
-
-        tnn_vector_dot = Dot(color=CIRCLE_COLOR)
-        tnn_vector = VMobject()
-        tnn_vector.add(
-            tnn_vector_dot,
-            Line(
-                start=tnn_vector_dot.get_right(),
-                end=tnn_vector_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-        )
-        tnn_vector.shift(DOWN)
-
-        tnn_matrix_dot = Dot(color=CIRCLE_COLOR)
-        tnn_matrix = VMobject()
-        tnn_matrix.add(
-            tnn_matrix_dot,
-            Line(
-                start=tnn_matrix_dot.get_right(),
-                end=tnn_matrix_dot.get_right() + INITIAL_LEG_LENGTH * RIGHT,
-            ),
-            Line(
-                start=tnn_matrix_dot.get_left(),
-                end=tnn_matrix_dot.get_left() + INITIAL_LEG_LENGTH * LEFT,
-            ),
-        )
-        tnn_matrix.shift(spacing_horizontal * RIGHT + DOWN)
-
-        # Text: - dots      (number)
-        #       - and lines (vector, matrix)
-        #       <small pause>
-        #       - and more lines connecting them    (trace, dot product)
-
-        self.play(Create(tnn_number), run_time=0.5)
-        self.wait(0.5)
-        self.play(Create(tnn_vector), run_time=0.5)
-        self.wait(0.5)
-        self.play(Create(tnn_matrix), run_time=0.5)
-        self.wait(0.5)
-
-        self.wait(1)
-
-        # Text: The shape won't matter, the number of legs does.
-        #       Thus, we will sometimes draw the center as
-        #       - a circle
-        #       - a square
-        #       - a diamond
-
-        tnn_vector_square = Square(
-            side_length=DEFAULT_DOT_RADIUS * 2, color=SQUARE_COLOR, fill_opacity=1.0
-        )
-        tnn_vector_square.move_to(tnn_vector_dot.get_center())
-        self.play(Transform(tnn_vector_dot, tnn_vector_square))
-
-        tnn_matrix_square = Square(
-            side_length=DEFAULT_DOT_RADIUS * 2, color=SQUARE_COLOR, fill_opacity=1.0
-        )
-        tnn_matrix_square.rotate(PI / 4)
-        tnn_matrix_square.move_to(tnn_matrix_dot.get_center())
-        self.play(Transform(tnn_matrix_dot, tnn_matrix_square))
-
-        self.wait(1)
+        new_body.move_to(original_body.get_center())
+        self.play(Transform(original_body, new_body))
 
 
 class SimpleActions(Scene):
