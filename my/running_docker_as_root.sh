e39b0bf4b29abaf7d16d57eb36164e1722324297
Dor Harpaz <harpazdor@gmail.com>
2023-09-17 14:46:07 +0300

    track docker setup

---
+++ my/running_docker_as_root.sh
@@ -0,0 +1,3 @@
+docker run -it -v /home/me/Sources/Python/manim/my/projects:/manim/my_projects -v /home/me/Sources/Python/manim/my/media:/manim/media --user root manimcommunity/manim:latest
+
+tlmgr install braket
